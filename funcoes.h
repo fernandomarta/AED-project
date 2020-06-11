8491041af89d01d0a9a554fe73afca2e3f146180
joaoeespada <joaodany@gmail.com>
2020-06-11 18:19:44 +0100

    menu a funcionar

--- funcoes.h
+++ funcoes.h
@@ -18,6 +18,31 @@ struct node *head = NULL;
 struct node *last = NULL;
 struct node *current = NULL;
 
+int num_selecionado;
+
+// FUNÇÃO MENU
+int menu()
+{
+   int opcao_f;
+
+   system("cls");
+    printf(" =============================================================================\n");
+    printf("                            Avaliacao de Alunos\n");
+    printf(" =============================================================================\n\n");
+    printf("                          <<<  Menu Principal >>>\n\n");
+    printf("                            1) Registar aluno \n");
+    printf("                            2) Editar registo\n");
+    printf("                            3) Eliminar registo\n");
+    printf("                            4) Pesquisar registo\n");
+    printf("                            5) Listar registos\n");
+    printf("                            0) Terminar Programa\n\n");
+    printf(" =============================================================================\n");
+    printf(" =============================================================================\n\n");
+    printf("Escolha a opcao: ");
+    scanf("%d", &opcao_f);
+
+    return(opcao_f);
+}
 
 //FUNÇÃO ESPERA
 
@@ -29,36 +54,36 @@ void espera ()
 
 // FUNÇAO REGISTAR
 
- void registar (node_t * head , char nome_aluno, float nota_frequencia, float nota_trabalho ) 
+ void registar (node_t **head , char nome_aluno, float nota_frequencia, float nota_trabalho ) 
  {
 
-//int num_aluno = 0;
-//float media_aluno = 0;
+   //int num_aluno = 0;
+   //float media_aluno = 0;
 
-/* Posicionar no nó inicial */
-   node_t *current = head;
+   /* Posicionar no nó inicial */
+      node_t *current = head;
 
-   
-/* Percorrer a lista até ao final */
-while (current->next != NULL)
-{
-current = current->next;
-}
+      
+   /* Percorrer a lista até ao final */
+   while (current->next != NULL)
+   {
+   current = current->next;
+   }
 
-/* Criar novo nó e adicionar no final */
-current->next = (node_t *) malloc(sizeof (node_t));
-current->next->num_aluno = current->num_aluno + 1;
-current->next->nome_aluno = nome_aluno;
-current->next->nota_frequencia = nota_frequencia;
-current->next->nota_trabalho = nota_trabalho;
-current->next->next = NULL;
+   /* Criar novo nó e adicionar no final */
+   current->next = (node_t *) malloc(sizeof (node_t));
+   current->next->num_aluno = current->num_aluno + 1;
+   current->next->nome_aluno = nome_aluno;
+   current->next->nota_frequencia = nota_frequencia;
+   current->next->nota_trabalho = nota_trabalho;
+   current->next->next = NULL;
 
  }
 
 
 // FUNÇÃO LISTAR
 
-void listar (node_t * head )
+void listar (node_t * head)
 {
 node_t * current = head->next;
 
@@ -77,13 +102,37 @@ current = current->next;
 
 // FUNÇÃO ELIMINAR
 
-void eliminar (node_t * head , int num_aluno )
+void eliminar (node_t **head , int num_aluno )
 {
-node_t *current = head;
+   node_t *current = head;
+
+   // Percorrer a lista até encontrar 
+
+   while (current->next->num_aluno != num_aluno)
+   {
+   /* if(current->next == NULL)
+         printf("registo não encontrado");
+         break; */
+
+      current = current->next;
+   }
+
+   // eliminar o nó
+
+   current->next = current->next->next;
+
+   }
+
+
+// FUNÇÃO PESQUISAR
+
+void pesquisar (node_t * head , int num_aluno )
+{
+node_t *current = head->next;
 
 // Percorrer a lista até encontrar 
 
-while (current->next->num_aluno != num_aluno)
+while (current->num_aluno != num_aluno)
 {
   /* if(current->next == NULL)
       printf("registo não encontrado");
@@ -92,8 +141,119 @@ while (current->next->num_aluno != num_aluno)
    current = current->next;
 }
 
-// eliminar o nó
+printf ( "Num \t NOME \t\t Frequencia \t Trabalho \t Media \n");
 
-current->next = current->next->next;
+printf("%d \t", current->num_aluno);
+printf("%-20c \t", current->nome_aluno);
+printf("%.1f \t\t", current->nota_frequencia);
+printf("%.1f \t\t", current->nota_trabalho);
+printf("%.1f \n", (current->nota_frequencia + current->nota_trabalho)/2);
+current = current->next;
+}
+
+// FUNÇÃO EDITAR
+
+void editar (node_t * head , int num_aluno)
+{
+   char nome_aluno;
+   float nota_frequencia;
+   float nota_trabalho;
+
+   node_t *current = head->next;
+
+   // Percorrer a lista até encontrar 
+
+   while (current->num_aluno != num_aluno)
+   {
+   /* if(current->next == NULL)
+         printf("registo não encontrado");
+         break; */
+
+      current = current->next;
+   }
+
+   getchar();
+   printf("Indique o Nome do Aluno: ");
+   scanf("%c",  &nome_aluno);
+   while (1)
+   {
+   printf("Indique a nota da frequencia: ");
+   scanf("%f",  &nota_frequencia);
+   if( nota_frequencia<0 || nota_frequencia> 20)
+      printf("valor invalido");
+   else 
+      break;
+   }
+   getchar();
+   while(1)
+   {
+   printf("Indique a nota do trabalho: ");
+   scanf("%f",  &nota_trabalho);
+   if( nota_trabalho<0 || nota_trabalho> 20)
+      printf("valor invalido");
+   else 
+      break;
+   }
+
+   current->num_aluno = num_aluno;
+   current->nome_aluno = nome_aluno;
+   current->nota_frequencia = nota_frequencia;
+   current->nota_trabalho = nota_trabalho;
 
 }
+
+// FUNÇÃO PERGUNTAR
+int perguntar ()
+{
+   int num_selecionado;
+
+   printf("Indique o numero do aluno: ");
+   scanf("%d", &num_selecionado);
+
+   return(num_selecionado);
+}
+
+// FUNÇÃO AVANÇAR
+void avancar ()
+{
+   printf("\n prima <enter> para continuar");
+   getchar();
+}
+
+// FUNÇÃO INDICAR DADOS
+
+/*int indicar_dados()
+{
+   printf("Indique o Nome do Aluno: ");
+         scanf("%c",  &nome_a);
+         while (1)
+         {printf("Indique a nota da frequencia: ");
+         scanf("%f",  &nota_f);
+         if( nota_f<0 || nota_f> 20)
+            printf("valor invalido");
+         else 
+            break;
+         }
+         getchar();
+         while(1)
+         {
+         printf("Indique a nota do trabalho: ");
+         scanf("%f",  &nota_t);
+         if( nota_t<0 || nota_t> 20)
+            printf("valor invalido");
+         else 
+            break;
+         }
+         printf("Tem a certeza? (s/n): ");
+         getchar();
+         scanf("%c", &certeza);
+         if (certeza == 's')
+            registar( &head , nome_a, nota_f, nota_t );
+
+         else if (certeza == 'n')
+         {
+            printf("\n dados nao gravados \n");
+            avancar();
+            break;
+         }
+}*/
--- trabalho_grupo.c
+++ trabalho_grupo.c
@@ -5,47 +5,34 @@
 #include <stdbool.h>
 #include "funcoes.h"
 
-
-
 //is list empty
 bool isEmpty() {
    return head == NULL;
 }
 
 
-
-
 int main()
 {
 //setlocale(LC_ALL,"pl_PL.UTF-8");
+
+FILE * alunos;
+
+alunos = fopen("BDalunos.db","a+b");
+if(!alunos)
+  {  puts("erro ao abrir ficheiro");
+    exit(1);
+  }
 
 int opcao=0;
 char nome_a;
 float nota_f, nota_t ;
 char certeza = 'n';
-int num_eliminar;
-
-
+int num_selecionado;
 
 while (1)
 {
 
-    
-    system("cls");
-    printf(" =============================================================================\n");
-    printf("                            Avaliacao de Alunos\n");
-    printf(" =============================================================================\n\n");
-    printf("                          <<<  Menu Principal >>>\n\n");
-    printf("                            1) Registar aluno \n");
-    printf("                            2) Editar registo\n");
-    printf("                            3) Eliminar registo\n");
-    printf("                            4) Pesquisar registo\n");
-    printf("                            5) Listar registos\n");
-    printf("                            0) Terminar Programa\n\n");
-    printf(" =============================================================================\n");
-    printf(" =============================================================================\n\n");
-    printf("Escolha a opcao: ");
-    scanf("%d", &opcao);
+    opcao = menu();
     getchar();
 
 
@@ -75,29 +62,43 @@ while (1)
         printf("Tem a certeza? (s/n): ");
         getchar();
         scanf("%c", &certeza);
-        if (certeza== 's')
+        if (certeza == 's')
           registar( &head , nome_a, nota_f, nota_t );
 
-        else
+        else if (certeza == 'n')
         {
-          printf("\n dados nao gravados");
-          sleep (5);
+          printf("\n dados nao gravados \n");
+          avancar();
           break;
         }
         break;
 
       case 2:
-        printf("\n escolheu n 2");
+          num_selecionado = perguntar();
+          pesquisar(&head , num_selecionado);
+          printf("Tem a certeza? (s/n): ");
+          getchar();
+          scanf("%c", &certeza);
+          if (certeza == 's')
+          {
+            editar(&head , num_selecionado);
+          }
+          else
+          {
+            system("cls");
+          }
+
         break;
 
       case 3:
-        printf( "Indique o num a eliminar");
-        scanf("%d", &num_eliminar);
-        eliminar (&head , num_eliminar);
+        num_selecionado = perguntar();
+        eliminar(&head , num_selecionado);
         break;
 
       case 4:
-        printf("\n escolheu n 4");
+        num_selecionado = perguntar();
+        pesquisar(&head, num_selecionado);
+        avancar();
         break;
 
       case 5:
@@ -118,10 +119,12 @@ while (1)
       default:
           printf("\n !!! A opcao escolhida nao e valida !!!\n");
           puts(" Por favor escolha uma opcao indicada");
-          printf("\n prima <enter> para continuar");
-          getchar();
+          avancar();
     }
 
  }
+
+fclose(alunos);
+
 return(0);
 }
